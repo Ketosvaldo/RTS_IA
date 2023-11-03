b0240dcbfcbd8bd90b968845757eba562a65564c
GilbertoSSJ <113067525+GilbertoSSJ@users.noreply.github.com>
2023-11-02 23:52:36 -0600

    CamaraMovimientoBasico

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,32 @@
+
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float panSpeed = 20f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+      if (Input.GetKey("w"))
+      {
+            pos.z += panSpeed * Time.deltaTime;
+      }
+      if (Input.GetKey("s"))
+      {
+            pos.z -= panSpeed * Time.deltaTime;
+      }
+      if (Input.GetKey("a"))
+      {
+            pos.x -= panSpeed * Time.deltaTime;
+      }
+      if (Input.GetKey("d"))
+      {
+            pos.x += panSpeed * Time.deltaTime;
+      }
+
+        transform.position = pos;
+    }
+}
